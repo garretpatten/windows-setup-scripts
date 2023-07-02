f893bc0f1dcaeee21406d2c59a6ccebc5b2b19bc
Garret Patten <garret.patten@proton.me>
2023-07-02 16:20:53 -0400

    Update addTasks.sh

--- src/scripts/addTasks.sh
+++ src/scripts/addTasks.sh
@@ -3,14 +3,13 @@
 # Add non-automated tasks to Taskwarrior
 if [[ -f "/usr/bin/task" ]]; then
 	# High Priority Tasks
-	task add Install Timeshift project:setup priority:H
 	task add Take a snapshot of system project:setup priority:H
 	task add Export GitHub PAT with 1Password project:dev priority:H
 
 	# Medium Priority Tasks
 	task add Sign into and sync Brave project:setup priority:M
 	task add Sign into Firefox project:setup priority:M
-	task add Look into Gnome tweaks project:setup priority:M
+	task add Customize look and feel of Windows environment
 
 	# Low Priority Tasks
 	task add Install Burp Suite project:setup priority:L
