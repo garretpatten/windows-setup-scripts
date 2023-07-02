885d506035b49e854c985c1937e804b5ba9abe05
Garret Patten <garret.patten@proton.me>
2023-07-02 18:43:09 -0400

    Update preinstall.sh

--- src/scripts/preinstall.sh
+++ src/scripts/preinstall.sh
@@ -3,3 +3,8 @@
 # Allow Script Execution
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
+# System Updates
+winget upgrade -h --all
+wsl
+sudo apt upgrade -y && sudo apt update-y && sudo apt autoremove -y
+exit
