b90bba1b6c26722729c976cf87488c7250cd7124
Garret Patten <garret.patten@proton.me>
2023-07-02 16:23:11 -0400

    Update misc.sh

--- src/scripts/misc.sh
+++ src/scripts/misc.sh
@@ -2,10 +2,7 @@
 if [[ -f "/usr/bin/thunderbird" ]]; then
  	echo "Thunderbird is already installed."
  else
-	sudo dnf install thunderbird -y
+	sudo apt install thunderbird -y
 fi
 
-# VLC
-# TODO: Below line is failing to parse
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install vlc -y
+# TODO: Install VLC
