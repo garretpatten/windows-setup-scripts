591f31961b2019ca672a60b1f757295489263412
Garret Patten <garret.patten@proton.me>
2023-07-02 19:10:42 -0400

    Update web.sh

--- src/scripts/web.sh
+++ src/scripts/web.sh
@@ -1,5 +1,8 @@
 # TODO: Install Brave
+winget install -e --id Brave.Brave
 
 # TODO: Install DuckDuckGo
+winget install -e --id DuckDuckGo.DesktopBrowser
 
 # TODO: Install Firefox
+winget install -e --id Mozilla.Firefox
