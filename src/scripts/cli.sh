5a69720f0fa1ac592b9edf2bc5fe0c954f66228d
Garret Patten <garret.patten@proton.me>
2023-07-02 17:06:17 -0400

    Update cli.sh

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,5 +1,19 @@
-# Install basic CLI tools
 cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
+for cliTool in ${cliTools[@]}; do
+	# TODO: Update filepath checks
+	if [[ -d "/usr/bin/$cliTool/" ]]; then
+		echo "$cliTool is already installed."
+	elif [ -f "/usr/sbin/$cliTool"]; then
+		echo "$cliTool is already installed."
+	else
+		winget install "$cliTool"
+	fi
+done
+
+# Switch to Windows Subsystem for Linux
+wsl
+
+# Install basic CLI tools on Windows Subsystem for Linux
 for cliTool in ${cliTools[@]}; do
 	if [[ -d "/usr/bin/$cliTool/" ]]; then
 		echo "$cliTool is already installed."
@@ -9,3 +23,6 @@ for cliTool in ${cliTools[@]}; do
 		sudo apt install "$cliTool" -y
 	fi
 done
+
+# Switch to Powershell
+exit
