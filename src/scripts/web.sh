cc8d8d1d359df952f4968a90f640464d74eed713
Garret Patten <garret.patten@proton.me>
2023-08-29 22:33:34 -0400

    Update web.sh

--- src/scripts/web.sh
+++ src/scripts/web.sh
@@ -1,8 +1,11 @@
-# TODO: Install Brave
-winget install -e --id Brave.Brave
-
-# TODO: Install DuckDuckGo
-winget install -e --id DuckDuckGo.DesktopBrowser
-
-# TODO: Install Firefox
-winget install -e --id Mozilla.Firefox
+# Install Brave, DuckDuckGo, Firefox
+apps=("Brave.Brave" "DuckDuckGo.DesktopBrowser" "Mozilla.Firefox")
+for app in ${apps[@]}; do
+	if [[ -d "/var/lib/flatpak/app/$apps" ]]; then
+		echo "$apps is already installed."
+	elif [[ -d "~/.local/share/flatpak/app/$flatpakApp" ]]; then
+		echo "$apps is already installed."
+	else
+		winget install -e --id Automattic.Simplenote "$app" -y
+	fi
+done
