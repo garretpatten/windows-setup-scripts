0a15eac04e3598bddeb1eda3479fe9bb3ab63782
Garret Patten <garret.patten@proton.me>
2023-08-29 22:24:10 -0400

    Update cli.sh

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,12 +1,13 @@
-cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
-for cliTool in ${cliTools[@]}; do
+# Install basic CLI tools for Powershell
+powershellCliTools=("sharkdp.bat" "GitHub.cli" "nepnep.neofetch-win" "OpenVPNTechnologies.OpenVPN")
+for powershellCliTool in ${powershellCliTools[@]}; do
 	# TODO: Update filepath checks
-	if [[ -d "/usr/bin/$cliTool/" ]]; then
-		echo "$cliTool is already installed."
-	elif [ -f "/usr/sbin/$cliTool"]; then
-		echo "$cliTool is already installed."
+	if [[ -d "/usr/bin/$powershellCliTool/" ]]; then
+		echo "$powershellCliTool is already installed."
+	elif [ -f "/usr/sbin/$powershellCliTool"]; then
+		echo "$powershellCliTool is already installed."
 	else
-		winget install "$cliTool"
+		winget install "$powershellCliTool"
 	fi
 done
 
@@ -15,6 +16,7 @@ winget install -e --id Python.Python.3.8
 wsl
 
 # Install basic CLI tools on Windows Subsystem for Linux
+cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
 for cliTool in ${cliTools[@]}; do
 	if [[ -d "/usr/bin/$cliTool/" ]]; then
 		echo "$cliTool is already installed."
