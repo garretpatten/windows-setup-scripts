bea0bef31ce4fdeaacdcfc80d48d3bcb7c3e3168
Garret Patten <garret.patten@proton.me>
2023-09-03 13:58:55 -0400

    Update cli.sh
    Remove Windows CLI tools and add to Kali tools

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,22 +1,7 @@
-# Install basic CLI tools for Powershell
-powershellCliTools=("sharkdp.bat" "GitHub.cli" "nepnep.neofetch-win" "OpenVPNTechnologies.OpenVPN")
-for powershellCliTool in ${powershellCliTools[@]}; do
-	# TODO: Update filepath checks
-	if [[ -d "/usr/bin/$powershellCliTool/" ]]; then
-		echo "$powershellCliTool is already installed."
-	elif [ -f "/usr/sbin/$powershellCliTool"]; then
-		echo "$powershellCliTool is already installed."
-	else
-		winget install "$powershellCliTool"
-	fi
-done
+wsl && cd
 
-winget install -e --id Python.Python.3.8
-
-wsl
-
-# Install basic CLI tools on Windows Subsystem for Linux
-cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "wget")
+# Install CLI tools on Windows Subsystem for Linux
+cliTools=("bat" "curl" "exa" "git" "https" "htop" "neofetch" "openvpn" "poppler-utils" "wget")
 for cliTool in ${cliTools[@]}; do
 	if [[ -d "/usr/bin/$cliTool/" ]]; then
 		echo "$cliTool is already installed."
